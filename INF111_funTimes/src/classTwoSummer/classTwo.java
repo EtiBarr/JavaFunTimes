271dfc502e194eed0c9ad246ff8d1c042e4e6e2d
EtiBarr <etienne.barriere.1@ens.etsmtl.ca>
2024-05-15 09:57:32 -0400

    ok

--- INF111_funTimes/src/classTwoSummer/classTwo.java
+++ INF111_funTimes/src/classTwoSummer/classTwo.java
@@ -1,4 +1,26 @@
 package INF111_funTimes.src.classTwoSummer;
 
 public class classTwo {
-}
+
+    public static void main(String[] args) {
+
+
+        int i = 3;
+
+        int x = 10;
+
+        switch(i) {
+
+            case 0: case 1: case 2: break;
+
+            case 3: i += 2;
+
+            case 4: x += i; break;
+
+            default: x = 3;
+
+        }
+
+        System.out.println("i: " + i + "x:" + x);
+    }
+    }
